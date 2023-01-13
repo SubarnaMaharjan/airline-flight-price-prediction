eb8c8d70586181d6408fb8d994ecf1a8b7a16417
avhishekpandey <ajaxpandey1234@gmail.com>
2023-01-13 13:57:54 +0545

    Minor Changes

--- EDA.py
+++ EDA.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from feature_eng import data
+import feature_eng as fe
+
+data = pd.read_excel(r'/home/vylericd3vil/Minor-project/Dataset/Data_Train.xlsx')
 
 sns.catplot(y = "Price", x = "Airline", data = data.sort_values("Price", ascending = False), kind="boxen", height = 6, aspect = 3)
 plt.show()
